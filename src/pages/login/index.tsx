524a809fb1de5126d67bdae365ec92d5ec69ce4f
Yago Felipe <yago-sigui9@hotmail.com>
2021-01-11 10:58:53 -0300

    project initial base

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+import React, { Component } from 'react';
+import { Container } from
+
+  class Login extends Component {
+
+    render() {
+      return (
+        <Container>
+
+        </Container>
+      );
+    }
+  }
---
+++ src/styles/pages/Home.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  color: #8257e5;
+`;
+
+export const View = styled.div`
+  
+`;
+
+export const Text = styled.p`
+  
+`;
+
+export const Button = styled.button`
+
+`;
